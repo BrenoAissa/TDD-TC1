0b453fc603804f5ca7de026c6e6bad3d6f133a0d
brenoaissa <brenoaugusto05@hotmail.com.br>
2025-04-21 22:13:13 -0300

    test: add initial tests for DroneDrive class

---
+++ src/test/java/org/ifsp/DroneDriveTest.java
@@ -0,0 +1,11 @@
+package org.ifsp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class DroneDriveTest {
+
+    @Test
+    void caixaPassaPelaJanela() {
+    }
+}
