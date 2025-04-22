bd2f296f092dff3c40802380b554c65613023767
brenoaissa <brenoaugusto05@hotmail.com.br>
2025-04-21 22:20:22 -0300

    test: implement caixaPassaPelaJanela test with multiple scenarios.

--- src/test/java/org/ifsp/DroneDriveTest.java
+++ src/test/java/org/ifsp/DroneDriveTest.java
@@ -7,5 +7,12 @@ class DroneDriveTest {
 
     @Test
     void caixaPassaPelaJanela() {
+        assertTrue(DroneDrive.caixaPassaPelaJanela(2, 3, 5, 3, 4), "Face AxB deveria passar");
+        assertTrue(DroneDrive.caixaPassaPelaJanela(2, 5, 3, 3, 4), "Face AxC deveria passar");
+        assertTrue(DroneDrive.caixaPassaPelaJanela(5, 2, 3, 3, 4), "Face BxC deveria passar");
+        assertFalse(DroneDrive.caixaPassaPelaJanela(5, 6, 7, 3, 4), "Nenhuma face deveria passar");
+        assertTrue(DroneDrive.caixaPassaPelaJanela(3, 4, 10, 3, 4), "Caixa deveria encaixar exatamente");
+        assertTrue(DroneDrive.caixaPassaPelaJanela(2, 5, 1, 5, 2), "Caixa deveria passar com janela rotacionada");
     }
+
 }
