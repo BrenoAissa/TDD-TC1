a574f79e51ddfdce64d4c91005c93651a379d7eb
brenoaissa <brenoaugusto05@hotmail.com.br>
2025-04-21 22:23:50 -0300

    test: add test for box being too large to fit through the window

--- src/test/java/org/ifsp/DroneDriveTest.java
+++ src/test/java/org/ifsp/DroneDriveTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class DroneDriveTest {
 
     @Test
-    void caixaPassaPelaJanela() {
+    void testCaixaPassaPelaJanela() {
         assertTrue(DroneDrive.caixaPassaPelaJanela(2, 3, 5, 3, 4), "Face AxB deveria passar");
         assertTrue(DroneDrive.caixaPassaPelaJanela(2, 5, 3, 3, 4), "Face AxC deveria passar");
         assertTrue(DroneDrive.caixaPassaPelaJanela(5, 2, 3, 3, 4), "Face BxC deveria passar");
@@ -16,12 +16,17 @@ class DroneDriveTest {
     }
 
     @Test
-    public void CaixaNaoPassa() {
+    public void testCaixaNaoPassa() {
         assertFalse(DroneDrive.caixaPassaPelaJanela(75, 100, 50, 100, 30));
     }
 
     @Test
     public void testeCaixaIgualJanela() {
         assertTrue(DroneDrive.caixaPassaPelaJanela(10, 10, 10, 10, 10));
+    }
+
+    @Test
+    public void testeCaixaNaoPassaMuitoGrande() {
+        assertFalse(DroneDrive.caixaPassaPelaJanela(100, 100, 100, 50, 50));
     }
 }
